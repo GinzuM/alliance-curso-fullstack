9ba47baa96f97785678535fc3098b2c567c20e94
GinzuM <rphsaraiva@gmail.com>
2021-07-28 22:10:46 -0300

    estamos quase acabando o desafio

--- dia3/frontend/src/components/ListaProdutos.vue
+++ dia3/frontend/src/components/ListaProdutos.vue
@@ -10,8 +10,25 @@
             <b-modal id="teste" title="Inserir novo produto"
                 ok-variant='success' cancel-variant="danger"
                 ok-title='Sim' cancel-title="Não"
+                v-on:ok="objetoProduto(codigoNovo, descricaoNovo, quantidadeNovo), adicionarProduto(novoProduto), created()"
+                
             >
-                <p> Insira os dados de um novo produto</p>
+
+            <b-form-group id="fieldset-1" label="Codigo"
+                label-for="input-1" :state="state">
+                <b-form-input id="input-1" type="Number" placeholder="CODIGO TESTE" v-model="codigoNovo" :state="state" ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="fieldset-2"  label="Descricao"
+                label-for="input-2" :state="state">
+                <b-form-input id="input-2" type="string" placeholder="CODIGO TESTE" v-model="descricaoNovo" :state="state" ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="fieldset-3" label="Quantidade"
+                label-for="input-3" :state="state">
+                <b-form-input id="input-3" type="number" placeholder="CODIGO TESTE" v-model="quantidadeNovo" :state="state" ></b-form-input>
+            </b-form-group>
+
             </b-modal>  
         </b-navbar>
         
@@ -32,8 +49,10 @@ export default {
     name: 'ListaProdutos',
     data: function(){
         return {
-            produtos: []
+            produtos: [],
+            novoProduto: {codigo: Number, descricao: String, quantidade:Number}
         }
+
     },
     components: {
         Produto
@@ -41,13 +60,26 @@ export default {
     methods: {
         removerProduto: function(index) {
             this.produtos.splice(index, 1)
+        },
+        objetoProduto: function(x,y,z){
+            this.novoProduto.codigo = x
+            this.novoProduto.descricao = y
+            this.novoProduto.quantidade = z
+        },
+        adicionarProduto: async function(novoProduto){
+            await axios.post('http://localhost:3000/produtos', novoProduto)
+        
         }
     },
+
     async created() {
         var response = await axios.get('http://localhost:3000/produtos')
         this.produtos = response.data
-    }
+    },
 }
+
+
+
 </script>
 
 <style scoped>
